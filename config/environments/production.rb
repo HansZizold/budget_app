dbce459fcd97af0f24cb777c20607109f4da984a
Hans Zizold <hzizold@hotmail.com>
2023-03-02 17:03:10 -0400

    Initial Setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.hosts << 'eshop-production-7696.up.railway.app'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
